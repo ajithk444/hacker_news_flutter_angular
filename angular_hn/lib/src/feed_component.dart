079cd8836d783ed74a8e807823f726f60771284e
Naoto Higuchi <ntaoo.g@gmail.com>
2018-04-16 21:41:35 +0900

    Add filtering input box.

--- angular_hn/lib/src/feed_component.dart
+++ angular_hn/lib/src/feed_component.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:angular_components/angular_components.dart';
 import 'package:model/model.dart';
 
 import 'item_component.dart';
@@ -9,7 +11,13 @@ import 'item_component.dart';
 @Component(
   selector: 'feed',
   templateUrl: 'feed_component.html',
-  directives: const [ItemComponent, NgFor, NgIf, routerDirectives],
+  directives: const [
+    ItemComponent,
+    NgFor,
+    NgIf,
+    routerDirectives,
+    MaterialInputComponent
+  ],
   pipes: const [AsyncPipe],
   encapsulation: ViewEncapsulation.None,
 )
@@ -17,6 +25,5 @@ class FeedComponent implements OnActivate {
   final HackerNewsService hackerNewsService;
   FeedComponent(this.hackerNewsService);
   @override
-  Future onActivate(_, __) async {
-  }
+  Future onActivate(_, __) async {}
 }
